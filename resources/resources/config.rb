883358a23f3513997c8308e46dae5b590be15c23
Miguel Negron <manegron@redborder.com>
2024-06-01 15:38:57 +0100

    lint resources

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -1,10 +1,8 @@
-# Cookbook Name:: rb-exporter
-#
+# Cookbook:: rb-exporter
 # Resource:: config
-#
 
 actions :add, :remove
 default_action :add
 
-attribute :user, :kind_of => String, :default => "rb-exporter"
-attribute :config_dir, :kind_of => String, :default => "/etc/rb-exporter"
+attribute :user, kind_of: String, default: 'rb-exporter'
+attribute :config_dir, kind_of: String, default: '/etc/rb-exporter'
